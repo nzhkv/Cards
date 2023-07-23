fcc9b9196ac158741ffdd0aa5bfcdb6f5a0b7644
NZhukov <nzhukov.rnd@gmail.com>
2023-07-23 14:51:52 +0400

    create Back card

--- Cards/ShapeLayer.playground/Contents.swift
+++ Cards/ShapeLayer.playground/Contents.swift
@@ -9,9 +9,11 @@ class MyViewController : UIViewController {
         view.backgroundColor = .white
         self.view = view
         
-        view.layer.addSublayer(CircleShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
-        view.layer.addSublayer(SquareShape(size: CGSize(width: 200, height: 100), fillColor: UIColor.green.cgColor))
-        view.layer.addSublayer(CrossShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.cyan.cgColor))
+//        view.layer.addSublayer(CircleShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
+//        view.layer.addSublayer(SquareShape(size: CGSize(width: 200, height: 100), fillColor: UIColor.green.cgColor))
+//        view.layer.addSublayer(CrossShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.cyan.cgColor))
+//        view.layer.addSublayer(BackSideCircle(size: CGSize(width: 200, height: 500), fillColor: UIColor.gray.cgColor))
+        view.layer.addSublayer(BackSideLine(size: CGSize(width: 200, height: 500), fillColor: UIColor.gray.cgColor))
     }
 }
 // Present the view controller in the Live View window
@@ -92,6 +94,62 @@ class FillShape: CAShapeLayer, ShapeLayerProtocol {
         let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         self.path = path.cgPath
         self.fillColor = fillColor
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        
+        for _ in 1...150 {
+            
+            let randomX = Int.random(in: 0...Int(size.width))
+            let randomY = Int.random(in: 0...Int(size.height))
+            let center = CGPoint(x: randomX, y: randomY)
+            
+            path.move(to: center)
+            let radius = Int.random(in: 2...12)
+            path.addArc(withCenter: center,
+                        radius: CGFloat(radius),
+                        startAngle: 0, endAngle: .pi * 2,
+                        clockwise: true)
+            
+        }
+        self.path = path.cgPath
+        
+        self.strokeColor = fillColor
+        self.fillColor = fillColor
+        self.lineWidth = 5
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class BackSideLine: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        let path = UIBezierPath()
+        
+        for _ in 1...50 {
+            let randomXStart = Int.random(in: 0...Int(size.width))
+            let randomYStart = Int.random(in: 0...Int(size.height))
+            
+            let randomXEnd = Int.random(in: 0...Int(size.width))
+            let randomYEnd = Int.random(in: 0...Int(size.height))
+            
+            path.move(to: CGPoint(x: randomXStart, y: randomXEnd))
+            path.addLine(to: CGPoint(x: randomXEnd, y: randomYEnd))
+        }
+        self.path = path.cgPath
+        self.strokeColor = fillColor
+        self.lineWidth = 3
+        self.lineCap = .round
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
