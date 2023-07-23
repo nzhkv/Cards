51b2f6a761bf0f705fc35b2c39248eda80124670
NZhukov <nzhukov.rnd@gmail.com>
2023-07-23 14:03:44 +0400

    create shape

--- Cards/ShapeLayer.playground/Contents.swift
+++ Cards/ShapeLayer.playground/Contents.swift
@@ -7,15 +7,96 @@ class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
         self.view = view
+        
+        view.layer.addSublayer(CircleShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
+        view.layer.addSublayer(SquareShape(size: CGSize(width: 200, height: 100), fillColor: UIColor.green.cgColor))
+        view.layer.addSublayer(CrossShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.cyan.cgColor))
     }
 }
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()
+
+
+protocol ShapeLayerProtocol: CAShapeLayer {
+    init(size: CGSize, fillColor: CGColor)
+}
+
+extension ShapeLayerProtocol {
+    init() {
+        fatalError("init() не может быть использован для создания экземпляра")
+    }
+}
+
+class CircleShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let radius = ([size.width, size.height].min() ?? 0) / 2
+        let centerX = size.width / 2
+        let centreY = size.height / 2
+        
+        let path = UIBezierPath(arcCenter: CGPoint(x: centerX, y: centreY),
+                                radius: radius,
+                                startAngle: 0, endAngle: .pi * 2,
+                                clockwise: true)
+        path.close()
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class SquareShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let edgeSize = ([size.width, size.height].min() ?? 0)
+        let rect = CGRect(x: 0, y: 0, width: edgeSize, height: edgeSize)
+        let path = UIBezierPath(rect: rect)
+        path.close()
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class CrossShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: size.width, y: size.height))
+        path.move(to: CGPoint(x: size.width, y: 0))
+        path.move(to: CGPoint(x: size.width, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: size.height))
+        self.path = path.cgPath
+        self.strokeColor = fillColor
+        self.lineWidth = 5
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class FillShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        self.path = path.cgPath
+        self.fillColor = fillColor
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
