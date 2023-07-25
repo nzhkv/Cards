01989ce8d4e1e49be424062201b5af5c8d23c6f2
NZhukov <nzhukov.rnd@gmail.com>
2023-07-25 14:21:43 +0400

    save

--- Cards/ShapeLayer.playground/Contents.swift
+++ Cards/ShapeLayer.playground/Contents.swift
@@ -33,6 +33,7 @@ extension ShapeLayerProtocol {
 protocol FlippableView: UIView {
     var isFlipped: Bool { get set }
     var flipCompletionHandler: ((FlippableView) -> Void)? { get set }
+    func flip()
 }
 
 class CircleShape: CAShapeLayer, ShapeLayerProtocol {
@@ -166,12 +167,45 @@ class CardView<ShapeType: ShapeLayerProtocol>: UIView, FlippableView {
     var flipCompletionHandler: ((FlippableView) -> Void)?
     var color: UIColor!
     
+    private let margin: Int = 10
+    
+    lazy var frontSideView: UIView = self.getFrontSideView()
+    lazy var backSideView: UIView = self.getBackSidwView()
+    
     init(frame: CGRect, color: UIColor) {
         super.init(frame: frame)
         self.color = color
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func getFrontSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        let shapeView = UIView(frame: CGRect(x: margin, y: margin, width: Int(self.bounds.width) - margin * 2, height: Int(self.bounds.height) - margin * 2))
+        let shapeLayer = ShapeType(size: shapeView.frame.size, fillColor: color.cgColor)
+        shapeView.layer.addSublayer(shapeLayer)
+        
+        return view
+    }
+    
+    private func getBackSidwView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        
+        switch ["circle", "line"].randomElement()! {
+        case "circle":
+            let layer = BackSideCircle(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        case "line":
+            let layer = BackSideLine(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        default:
+            break
+        }
+        return view
     }
     
     func flip() {}
