a0fc11329c529433ee8eedef712d75de136b122a
NZhukov <nzhukov.rnd@gmail.com>
2023-07-28 12:56:22 +0400

    ver 1.0

--- Cards/Controller/BoardGameController.swift
+++ Cards/Controller/BoardGameController.swift
@@ -11,6 +11,7 @@ class BoardGameController: UIViewController {
     
     var cardsPairsCounts = 8
     var cardViews = [UIView]()
+    private var flippedCards = [UIView]()
     
     lazy var game: Game = getNewGame()
     lazy var startButtonView = getStartButton()
@@ -102,10 +103,39 @@ class BoardGameController: UIViewController {
         }
         
         for card in cardViews {
-            (card as! FlippableView).flipCompletionHandler = { flippedCard in
+            (card as! FlippableView).flipCompletionHandler = { [self] flippedCard in
                 flippedCard.superview?.bringSubviewToFront(flippedCard)
+                
+                if flippedCard.isFlipped {
+                    self.flippedCards.append(flippedCard)
+                } else {
+                    if let cardIndex = self.flippedCards.firstIndex(of: flippedCard) {
+                        self.flippedCards.remove(at: cardIndex)
+                    }
+                }
+                
+                if self.flippedCards.count == 2 {
+                    let firstCard = game.cards[self.flippedCards.first!.tag]
+                    let secondCard = game.cards[self.flippedCards.last!.tag]
+                    
+                    if game.checkCards(firstCard, secondCard) {
+                        UIView.animate(withDuration: 0.3, animations: {
+                            self.flippedCards.first!.layer.opacity = 0
+                            self.flippedCards.last!.layer.opacity = 0
+                        }, completion: {_ in
+                            self.flippedCards.first!.removeFromSuperview()
+                            self.flippedCards.last!.removeFromSuperview()
+                            self.flippedCards = []
+                        })
+                    } else {
+                        for card in self.flippedCards {
+                            (card as! FlippableView).flip()
+                        }
+                    }
+                }
             }
         }
+        
         return cardViews
     }
     
